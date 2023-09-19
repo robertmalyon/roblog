22aaac9cf3032f49e22a0cabd36f0bca3b795c51
Rob <rob.malyon@gmail.com>
2023-09-19 16:10:11 +0100

    Tried manually adding decap cms

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@ import { defineConfig } from 'astro/config'; // import lit from '@astrojs/lit';
 
 import sitemap from '@astrojs/sitemap';
 
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-minimal-starter.netlify.app/",
-  integrations: [sitemap()]
+  integrations: [sitemap(), tailwind()]
 });
